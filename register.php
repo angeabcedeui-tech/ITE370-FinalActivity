9638277ec4667b85e837074553693ed84c666174
angeabcedeui-tech <ange.abcede.ui@phinmaed.com>
2026-03-20 18:20:58 +0800

    Create register.php

---
+++ register.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../core/auth.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $response = registerUser($_POST['username'], $_POST['password']);
+
+    if ($response['success']) {
+        echo "Registered successfully";
+    } else {
+        echo $response['error'];
+    }
+}
+?>
+
+<form method="POST">
+    <input type="text" name="username">
+    <input type="password" name="password">
+    <button type="submit">Register</button>
+</form>="submit">Register</button>
+</form>
