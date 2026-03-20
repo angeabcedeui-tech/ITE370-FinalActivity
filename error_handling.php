6b945bb71707ef9780d40470ef80794eaed7491a
angeabcedeui-tech <ange.abcede.ui@phinmaed.com>
2026-03-20 18:16:17 +0800

    Update error_handling.php

--- error_handling.php
+++ error_handling.php
@@ -1,6 +1,16 @@
 <?php
-function handleError($message) {
-    echo "Error: " . $message;
-    exit();
+
+function handleError($e) {
+
+    if ($_ENV['APP_DEBUG'] === 'true') {
+        return [
+            "success" => false,
+            "error" => $e->getMessage()
+        ];
+    }
+
+    return [
+        "success" => false,
+        "message" => "Something went wrong"
+    ];
 }
-?>
