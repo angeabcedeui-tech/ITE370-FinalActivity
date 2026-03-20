ce38c25620ad7f141428eb36a3fae0bba55673d1
angeabcedeui-tech <ange.abcede.ui@phinmaed.com>
2026-03-20 18:18:41 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+require_once '.auth.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $response = loginUser($_POST['username'], $_POST['password']);
+
+    if ($response['success']) {
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        echo $response['message'];
+    }
+}
+?>
+
+<form method="POST">
+    <input type="text" name="username" placeholder="Username">
+    <input type="password" name="password" placeholder="Password">
+    <button type="submit">Login</button>
+</form>
