6c0dc61edc3b4fe203ae045558515032fd16cab7
angeabcedeui-tech <ange.abcede.ui@phinmaed.com>
2026-03-20 18:14:34 +0800

    Add input validation function to sanitize data

--- input_validation.php
+++ input_validation.php
@@ -1,5 +1,13 @@
-?php
-function sanitize($data) {
-    return htmlspecialchars(stripslashes(trim($data)));
+<?php
+
+function validateInput($data, $fieldName) {
+    if (empty($data)) {
+        throw new Exception("$fieldName is required");
+    }
+
+    if (strlen($data) < 3) {
+        throw new Exception("$fieldName must be at least 3 characters");
+    }
+
+    return htmlspecialchars(trim($data));
 }
-?>
